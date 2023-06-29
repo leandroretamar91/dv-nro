81df3feb8013cae308a2f688b7b0ad6bf3f2b5e9
baakudai <lea.retamar@gmail.com>
2023-06-28 23:14:15 -0300

    Create functions.js

---
+++ source/functions.js
@@ -0,0 +1,8 @@
+function navLateralAbrir() {
+  document.getElementById("nav-lat-bar").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function navLateralCerrar() {
+  document.getElementById("nav-lat-bar").style.width = "0";
+}
